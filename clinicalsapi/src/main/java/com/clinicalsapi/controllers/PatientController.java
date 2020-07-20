1fc26a2a914ceb883addb5228a90411fcac6260c
yash270998 <a00268810@student.ait.ie>
2020-07-20 16:06:15 +0100

    Rest Controller for Patient Data

---
+++ clinicalsapi/src/main/java/com/clinicalsapi/controllers/PatientController.java
@@ -0,0 +1,39 @@
+package com.clinicalsapi.controllers;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.clinicalsapi.entities.Patient;
+import com.clinicalsapi.repos.PatientRepository;
+
+@RestController
+@RequestMapping("/api")
+public class PatientController {
+
+	private PatientRepository patientRepo;
+
+	@Autowired
+	PatientController(PatientRepository patientRepository) {
+		this.patientRepo = patientRepo;
+	}
+
+	@RequestMapping(value = "/patients", method = RequestMethod.GET)
+	public List<Patient> getPatients() {
+		return patientRepo.findAll();
+	}
+	@RequestMapping(value = "/patients/{id}",method = RequestMethod.GET)
+	public Patient getPatient(@PathVariable("id") int id) {
+		return patientRepo.findById(id).get();
+	}
+	@RequestMapping(value = "/patients",method = RequestMethod.POST)
+	public Patient savePatient(Patient patient) {
+		return patientRepo.save(patient);
+	}
+}
