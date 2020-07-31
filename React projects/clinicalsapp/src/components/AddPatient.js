ab39a8d047f0fd67ecafacfb56fb6c2de3d4ceb9
yash270998 <yashsomani2007@gmail.com>
2020-07-31 13:49:22 +0100

    Initialized components

---
+++ React projects/clinicalsapp/src/components/AddPatient.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AddPatient extends React.Component{
+    render(){
+        return(<div>
+
+        </div>)
+    }
+}
+
+export default AddPatient;
---
+++ React projects/clinicalsapp/src/components/CollectClinicals.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CollectClinicals extends React.Component{
+    render(){
+        return(<div>
+
+        </div>)
+    }
+}
+
+export default CollectClinicals;
